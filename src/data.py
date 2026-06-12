16722097b17a6f7a1cb83bccdcf7fb09fc6f6f26
Bikesh <bikeshgyawali1@gmail.com>
2026-06-12 11:45:01 -0700

    refactor get_spy_data function to encapsulate data retrieval logic

--- src/data.py
+++ src/data.py
@@ -1,8 +1,9 @@
 import yfinance as yf
 import pandas as pd
 
-spy_data = yf.download("SPY", start="2025-01-01", end="2026-01-01")
+def get_spy_data():
 
-column_data = spy_data['Close'].squeeze().tolist()
+    spy_data = yf.download("SPY", start="2025-01-01", end="2026-01-01")
+    column_data = spy_data['Close'].squeeze().tolist()
 
 
