24d16091b8c9a1d7ae8f17f8a29670cf77a0a505
Bikesh <bikeshgyawali1@gmail.com>
2026-06-13 09:49:55 -0700

    update config loading to be more optimized

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from src.data import data
 from src.strategies import choose_random
 from src import config
 
-portfolio = config.load_config()["simulation"]["initial_portfolio"]
-shares = config.load_config()["simulation"]["initial_shares"]
-ticker = config.load_config()["data"]["ticker"]
-start_date = config.load_config()["data"]["start_date"]
-end_date = config.load_config()["data"]["end_date"]
+settings = config.load_config()
+
+portfolio = settings["simulation"]["initial_portfolio"]
+shares = settings["simulation"]["initial_shares"]
+ticker = settings["data"]["ticker"]
+start_date = settings["data"]["start_date"]
+end_date = settings["data"]["end_date"]
 
 
 def backtest(portfolio, shares, ticker, start_date, end_date):
