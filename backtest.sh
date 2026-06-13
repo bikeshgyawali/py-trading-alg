d1cbf905a1009d5cccbaebd62a96bd25e31d0a3f
Bikesh <bikeshgyawali1@gmail.com>
2026-06-13 10:02:59 -0700

    add requirements.txt and update bash script to match

--- backtest.sh
+++ backtest.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 if [ ! -d ".venv" ] && [ ! -d "venv" ]; then
     python3 -m venv .venv
     source .venv/bin/activate
+    pip install -r requirements.txt
 else
     source .venv/bin/activate
 fi
